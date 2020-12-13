ac9715f0e664f0637366015c6e2c6dbe9c15db90
lucas <wimpheling@gmail.com>
2020-12-13 22:41:48 +0100

    export UniqueInParentValueIndex

--- src/prelude/mod.rs
+++ src/prelude/mod.rs
@@ -2,3 +2,4 @@ mod validators;
 
 pub use validators::unique_in_parent::UniqueInParentPlugin;
 pub use validators::unique_in_parent::UniqueInParentValidator;
+pub use validators::unique_in_parent::UniqueInParentValueIndex;
--- src/prelude/validators/unique_in_parent/mod.rs
+++ src/prelude/validators/unique_in_parent/mod.rs
@@ -2,6 +2,7 @@ mod unique_in_parent_plugin;
 mod unique_in_parent_validator;
 
 pub use unique_in_parent_plugin::UniqueInParentPlugin;
+pub use unique_in_parent_plugin::UniqueInParentValueIndex;
 pub use unique_in_parent_validator::UniqueInParentValidator;
 
 #[cfg(test)]
