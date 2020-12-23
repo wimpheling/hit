54bf5cdf35dfc4a1687c4094ee8b8ef622a34fbc
lucas <wimpheling@gmail.com>
2020-12-23 01:50:28 +0100

    restore hit_move_helper

--- src/hit_mod/helpers/hit_move_helper.rs
+++ src/hit_mod/helpers/hit_move_helper.rs
@@ -1,5 +1,4 @@
-/* use crate::field_types::FieldTypeSubobjectArray;
- */
+use crate::field_types::FieldTypeSubobjectArray;
 use crate::index::can_move_object as index_can_move_object;
 use crate::model::Model;
 use crate::IndexEntryProperty;
@@ -9,35 +8,35 @@ use std::rc::Rc;
 // TODO : that's not generic, it should use the allow_model method
 // this prevents from creating custom subobject fields with custom rules :/
 fn _can_move_object(
-    _index: &Hit,
-    _id: &str,
-    _target_model: Rc<Model>,
-    _property: &str,
+    index: &Hit,
+    id: &str,
+    target_model: Rc<Model>,
+    property: &str,
 ) -> Result<(), HitError> {
     // TODO : should this be implemented ?
-    /* let model = index
-         .get_model(id)
-         .ok_or(HitError::NoModelForId(id.to_string()))?;
-     let target_field = target_model
-         .get_field(property)
-         .ok_or(HitError::PropertyNotFound(property.to_string()))?
-         .borrow();
+    let model = index
+        .get_model(id)
+        .ok_or(HitError::NoModelForId(id.to_string()))?;
+    let target_field = target_model
+        .get_field(property)
+        .ok_or(HitError::PropertyNotFound(property.to_string()))?
+        .borrow();
     let target_field = target_field
-         .downcast_ref::<FieldTypeSubobjectArray>()
-         .ok_or(HitError::InvalidMoveDestination())?;
-       for allowed_model in target_field.authorized_models.iter() {
-         if allowed_model == model.get_name() {
-             return Ok(());
-         }
-         for interface in model.interfaces.iter() {
-             if allowed_model == interface {
-                 return Ok(());
-             }
-         }
-     }
-     Err(HitError::ModelNotAllowed(model.get_name().clone()))*/
-    Ok(())
+        .downcast_ref::<FieldTypeSubobjectArray>()
+        .ok_or(HitError::InvalidMoveDestination())?;
+    for allowed_model in target_field.authorized_models.iter() {
+        if allowed_model == model.get_name() {
+            return Ok(());
+        }
+        for interface in model.interfaces.iter() {
+            if allowed_model == interface {
+                return Ok(());
+            }
+        }
+    }
+    Err(HitError::ModelNotAllowed(model.get_name().clone()))
 }
+
 pub fn can_move_object(
     index: &Hit,
     id: &str,
